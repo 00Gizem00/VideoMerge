30c0ce880ee39918b2030276cf4729e23ab98280
00Gizem00 <gizem_fbl@windowslive.com>
2024-04-11 14:46:31 +0300

    videolar birlesti. boyut degisikliginden dolayi bir birlesme hatasi var. cozulecek

--- video_merger.py
+++ video_merger.py
@@ -1,14 +1,41 @@
-from moviepy.editor import *
+import os
+import subprocess
 
-clip1 = VideoFileClip("video_list/video1.mp4")
-clip2 = VideoFileClip("video_list/video2.mp4")
 
-final_clip = concatenate_videoclips([clip1, clip2], method="compose")
+klasor = input("Klasör yolunu girin: ")
 
-final_clip.write_videofile(
-    "combined2_video.mp4",
-    codec="libx264",
-    audio_codec="aac",
-    temp_audiofile="temp-audio.m4a",
-    remove_temp=True,
-)
+
+print("Klasördeki dosyalar:")
+dosyalar = os.listdir(klasor)
+for i, dosya in enumerate(dosyalar, start=1):
+    print(f"{i}. {dosya}")
+
+
+secilenler = input("Seçmek istediğiniz dosyaların numaralarını aralarında boşluk bırakarak girin (örn: 1 3 5): ")
+secilenler = [int(index) for index in secilenler.split()]
+
+
+secilen_dosyalar = [dosyalar[index - 1] for index in secilenler]
+
+
+birlesmis_dosya = 'birlesmis_dosya.mp4'
+
+
+with open('concat_list.txt', 'w') as f:
+    for dosya in secilen_dosyalar:
+        dosya_yolu = os.path.join(klasor, dosya)
+        f.write(f"file '{dosya_yolu}'" + '\n')
+
+
+ffmpeg_cmd = ['ffmpeg', '-y', '-f', 'concat', '-safe', '0', '-i', 'concat_list.txt', '-c', 'copy', birlesmis_dosya]
+process = subprocess.run(ffmpeg_cmd, stderr=subprocess.PIPE)
+
+# Hataları log dosyasına yaz
+with open('ffmpeg_error.log', 'a') as f:
+    f.write(process.stderr.decode())
+
+
+os.remove('concat_list.txt')
+
+print(f"Videolar birleştirildi ve '{birlesmis_dosya}' adında bir dosya oluşturuldu.")
+
